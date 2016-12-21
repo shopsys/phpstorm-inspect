1663d04b9d811f2852ed23241860fd67db80cceb
Cyril <cyril.sadovnik@gmail.com>
2016-12-21 10:38:41 +0300

    Adapted PSR-2 code style standard

--- src/ShopSys/PhpStormInspect/InspectionRunner.php
+++ src/ShopSys/PhpStormInspect/InspectionRunner.php
@@ -4,76 +4,78 @@ namespace ShopSys\PhpStormInspect;
 
 use Symfony\Component\Filesystem\Filesystem;
 
-class InspectionRunner {
+class InspectionRunner
+{
+    const CACHE_DIR = 'caches';
+    const INDEX_DIR = 'index';
 
-	const CACHE_DIR = 'caches';
-	const INDEX_DIR = 'index';
+    /**
+     * @var \Symfony\Component\Filesystem\Filesystem
+     */
+    private $filesystem;
 
-	/**
-	 * @var \Symfony\Component\Filesystem\Filesystem
-	 */
-	private $filesystem;
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
 
-	public function __construct(Filesystem $filesystem) {
-		$this->filesystem = $filesystem;
-	}
+    public function clearCache($phpstormSystemPath) {
+        if (is_dir($phpstormSystemPath . '/' . self::CACHE_DIR)) {
+            $this->clearDirectory($phpstormSystemPath . '/' . self::CACHE_DIR);
+        }
 
-	public function clearCache($phpstormSystemPath) {
-		if (is_dir($phpstormSystemPath . '/' . self::CACHE_DIR)) {
-			$this->clearDirectory($phpstormSystemPath . '/' . self::CACHE_DIR);
-		}
+        if (is_dir($phpstormSystemPath . '/' . self::INDEX_DIR)) {
+            $this->clearDirectory($phpstormSystemPath . '/' . self::INDEX_DIR);
+        }
+    }
 
-		if (is_dir($phpstormSystemPath . '/' . self::INDEX_DIR)) {
-			$this->clearDirectory($phpstormSystemPath . '/' . self::INDEX_DIR);
-		}
-	}
+    public function clearOutputDirectory($outputPath)
+    {
+        $files = glob($outputPath . '/*.xml');
+        $this->filesystem->remove($files);
+    }
 
-	public function clearOutputDirectory($outputPath) {
-		$files = glob($outputPath . '/*.xml');
-		$this->filesystem->remove($files);
-	}
+    /**
+     * @param string $inspectShExecutableFilepath
+     * @param string $projectPath
+     * @param string $inspectionProfileFilepath
+     * @param string $outputPath
+     * @param string $inspectedDirectory
+     */
+    public function runInspection(
+        $inspectShExecutableFilepath,
+        $projectPath,
+        $inspectionProfileFilepath,
+        $outputPath,
+        $inspectedDirectory
+    ) {
+        $command = sprintf(
+            '%s %s %s %s -d %s 2>&1',
+            escapeshellarg($inspectShExecutableFilepath),
+            escapeshellarg($projectPath),
+            escapeshellarg($inspectionProfileFilepath),
+            escapeshellarg($outputPath),
+            escapeshellarg($inspectedDirectory)
+        );
 
-	/**
-	 * @param string $inspectShExecutableFilepath
-	 * @param string $projectPath
-	 * @param string $inspectionProfileFilepath
-	 * @param string $outputPath
-	 * @param string $inspectedDirectory
-	 */
-	public function runInspection(
-		$inspectShExecutableFilepath,
-		$projectPath,
-		$inspectionProfileFilepath,
-		$outputPath,
-		$inspectedDirectory
-	) {
-		$command = sprintf(
-			'%s %s %s %s -d %s 2>&1',
-			escapeshellarg($inspectShExecutableFilepath),
-			escapeshellarg($projectPath),
-			escapeshellarg($inspectionProfileFilepath),
-			escapeshellarg($outputPath),
-			escapeshellarg($inspectedDirectory)
-		);
+        $returnCode = null;
+        ob_start();
+        passthru($command, $returnCode);
+        $output = ob_get_clean();
 
-		$returnCode = null;
-		ob_start();
-		passthru($command, $returnCode);
-		$output = ob_get_clean();
+        // PhpStorm exits without error when another instance is already running
+        if ($returnCode === 0 && mb_strpos($output, 'Too Many Instances') !== false) {
+            $returnCode = 1;
+        }
 
-		// PhpStorm exits without error when another instance is already running
-		if ($returnCode === 0 && mb_strpos($output, 'Too Many Instances') !== false) {
-			$returnCode = 1;
-		}
+        if ($returnCode !== 0) {
+            throw new \Exception($output);
+        }
+    }
 
-		if ($returnCode !== 0) {
-			throw new \Exception($output);
-		}
-	}
-
-	private function clearDirectory($directory) {
-		$files = glob($directory . '/*');
-		$this->filesystem->remove($files);
-	}
-
+    private function clearDirectory($directory)
+    {
+        $files = glob($directory . '/*');
+        $this->filesystem->remove($files);
+    }
 }
